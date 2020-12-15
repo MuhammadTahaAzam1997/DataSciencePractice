4122c6138d1bccd99d519246ca7ce3cbc6ed79df
Taha Azam <muhammadtahaazam@gmail.com>
2020-12-15 15:30:25 +0500

    Numpy in python

--- dict.py
+++ dict.py
@@ -1,3 +1,6 @@
+#dictionaries in python
 l={'name':['Muhammad','Taha','Azam'],
    'age': [22,323,24]}
 print (l['name'][0])
+
+
---
+++ numpy.py
@@ -0,0 +1,6 @@
+#Numpy in Python
+import numpy as np
+import timeit
+
+a= np.loadtxt("dict.py")
+print (a)
